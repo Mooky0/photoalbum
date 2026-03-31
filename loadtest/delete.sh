2b67b7263c2fee726917d5b9ca61fb47181f8ca1
Gábor Tóth <tothgabor2003@gmail.com>
2026-03-31 06:19:01 +0200

    fix Photo attribute

--- loadtest/delete.sh
+++ loadtest/delete.sh
@@ -1,3 +1,3 @@
 POD=$(kubectl get pods -l app=photoalbum-backend -o jsonpath='{.items[0].metadata.name}')
 
-oc exec $POD -- python manage.py shell -c "from api.models import Photo; [ (p.file.delete(save=False), p.delete()) for p in Photo.objects.all() ]"
+oc exec $POD -- python manage.py shell -c "from api.models import Photo; [ (p.image.delete(save=False), p.delete()) for p in Photo.objects.all() ]"
